330a1f763ade74918e3d49844388a855669cef11
ideapad 5 pro greg <ideapad 5 pro greg@LAPTOP-BOLBPPBG>
2024-09-27 10:10:44 +0800

    Add project files.

---
+++ WinFormsAppvb/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtUsername = New TextBox()
+        txtPassword = New TextBox()
+        btnLogin = New Button()
+        lblMessage = New Label()
+        SuspendLayout()
+        ' 
+        ' txtUsername
+        ' 
+        txtUsername.Location = New Point(288, 64)
+        txtUsername.Name = "txtUsername"
+        txtUsername.PlaceholderText = "Username"
+        txtUsername.Size = New Size(200, 39)
+        txtUsername.TabIndex = 1
+        txtUsername.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtPassword
+        ' 
+        txtPassword.Location = New Point(288, 135)
+        txtPassword.Name = "txtPassword"
+        txtPassword.PasswordChar = "*"c
+        txtPassword.PlaceholderText = "Password"
+        txtPassword.Size = New Size(200, 39)
+        txtPassword.TabIndex = 3
+        txtPassword.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' btnLogin
+        ' 
+        btnLogin.Location = New Point(311, 204)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(150, 46)
+        btnLogin.TabIndex = 4
+        btnLogin.Text = "Login"
+        btnLogin.UseVisualStyleBackColor = True
+        ' 
+        ' lblMessage
+        ' 
+        lblMessage.AutoSize = True
+        lblMessage.Location = New Point(133, 351)
+        lblMessage.Name = "lblMessage"
+        lblMessage.Size = New Size(24, 32)
+        lblMessage.TabIndex = 5
+        lblMessage.Text = "_"
+        lblMessage.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Form1
+        ' 
+        AllowDrop = True
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lblMessage)
+        Controls.Add(btnLogin)
+        Controls.Add(txtPassword)
+        Controls.Add(txtUsername)
+        Name = "Form1"
+        Text = "Enter"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents lblMessage As Label
+
+End Class
---
+++ WinFormsAppvb/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Dim usernames() As String = {"mark", "rashed", "user3", "user4", "user5"}
+    Dim passwords() As String = {"austero", "carnain", "pass3", "pass4", "pass5"}
+    Dim attempts As Integer = 3
+
+    Private Function IsLoginValid(ByVal username As String, ByVal password As String) As Boolean
+        For i As Integer = 0 To usernames.Length - 1
+            If username = usernames(i) AndAlso password = passwords(i) Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        If attempts > 0 Then
+            Dim username As String = txtUsername.Text
+            Dim password As String = txtPassword.Text
+
+            If IsLoginValid(username, password) Then
+                lblMessage.Text = "LOGIN SUCCESSFUL!"
+                lblMessage.ForeColor = Color.Green
+            Else
+                attempts -= 1
+                lblMessage.Text = "INCORRECT USERNAME OR PASSWORD! " & attempts & " ATTEMPTS LEFT!"
+                lblMessage.ForeColor = Color.Red
+            End If
+
+            If attempts = 0 Then
+                btnLogin.Enabled = False
+                lblMessage.Text = "HACKER GET OUT!"
+            End If
+        End If
+    End Sub
+End Class
